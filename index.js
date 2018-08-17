c7d4382a2dcb5e3d0fdade98babc5daf360cc1df
henrikra <henrik.raitasola@gmail.com>
2018-08-17 09:07:54 +0300

    Add bottom tabs

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 import { Navigation } from "react-native-navigation";
 import App from "./App";
 
-Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+Navigation.registerComponent("navigation.playground.WelcomeScreen", () => App);
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
-      component: {
-        name: "navigation.playground.WelcomeScreen"
+      bottomTabs: {
+        children: [
+          {
+            stack: {
+              children: [
+                {
+                  component: {
+                    name: "navigation.playground.WelcomeScreen",
+                    options: { bottomTab: { text: "Messages" } }
+                  }
+                }
+              ]
+            }
+          },
+          {
+            component: {
+              name: "navigation.playground.WelcomeScreen",
+              options: { bottomTab: { text: "Home" } }
+            }
+          },
+          {
+            component: {
+              name: "navigation.playground.WelcomeScreen",
+              options: { bottomTab: { text: "Feed" } }
+            }
+          },
+          {
+            component: {
+              name: "navigation.playground.WelcomeScreen",
+              options: { bottomTab: { text: "Store" } }
+            }
+          }
+        ]
       }
     }
   });
